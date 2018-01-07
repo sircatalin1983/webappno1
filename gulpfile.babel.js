29cd41bbe9b3120648af60ebd6edc2440fba5a98
Catalin <mcatalin1983@yahoo.com>
2018-01-07 21:56:07 +0000

    fix

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -55,6 +55,25 @@ const paths = {
     dist: 'dist'
 };
 
+const arg = (argList => {
+  let arg = {}, a, opt, thisOpt, curOpt;
+  for (a = 0; a < argList.length; a++) {
+    thisOpt = argList[a].trim();
+    opt = thisOpt.replace(/^\-+/, '');
+
+    if (opt === thisOpt) {
+      // argument value
+      if (curOpt) arg[curOpt] = opt;
+      curOpt = null;
+    } else {
+      // argument name
+      curOpt = opt;
+      arg[curOpt] = true;
+    }
+  }
+  return arg;
+})(process.argv);
+
 /********************
  * Helper functions
  ********************/
@@ -455,33 +474,12 @@ gulp.task('test:client', done => {
 
 gulp.task('build-image', function(imageId) {
   // fetch command line arguments
-  const arg = (argList => {
-    let arg = {}, a, opt, thisOpt, curOpt;
-    for (a = 0; a < argList.length; a++) {
-      thisOpt = argList[a].trim();
-      opt = thisOpt.replace(/^\-+/, '');
-
-      if (opt === thisOpt) {
-        // argument value
-        if (curOpt) arg[curOpt] = opt;
-        curOpt = null;
-      } else {
-        // argument name
-        curOpt = opt;
-        arg[curOpt] = true;
-      }
-    }
-    return arg;
-  })(process.argv);
-
-  console.log('imageId: ' + imageId)
-
-  console.log('lista parametrii: ' + process.argv)
   console.log('imageId: '+ arg['imageId'])
 });
 
 gulp.task('deploy-image', function(targetEnv, imageId) {
-  console.log('imageId: ' + imageId)
+  console.log('targetEnv: '+ arg['targetEnv'])
+  console.log('imageId: '+ arg['imageId'])
 });
 
 
