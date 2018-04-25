c98499c02de9e40b21e6479d10629184f0c3edc1
Catalin <mcatalin1983@yahoo.com>
2018-04-25 20:02:05 +0000

    s

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -535,7 +535,7 @@ gulp.task('deploy-image', function () {
             + ' --env NODE_ENV=' + arg['targetEnv'] + ' webappno1:' + arg['imageId']);
 
             var rc = shell.exec('docker run -t -d --name webappno1-' + arg['targetEnv'] + ' --link webappno1db:mongo.server -p '
-                + ports[arg['targetEnv']] + ': 8080' 
+                + '9002 : 8080' 
                 + ' --env NODE_ENV=' + arg['targetEnv'] + ' webappno1:' + arg['imageId']).code;            
             if (rc > 0) {
                 console.log("DOCKER FAILURE");
